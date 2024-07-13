bbd8c17ba68c8e6cafd4c985210d5a434a0a34c6
ariful <hq.ariful@gmail.com>
2024-07-13 20:30:14 +0600

    fix: included the response module

--- details.py
+++ details.py
@@ -1,16 +1,10 @@
-import urllib.request as request
-from urllib.error import URLError, HTTPError
-import json
+import requests
 
 def wordMeaning(word):
-	# trying to read the URL
+	# trying to read the URL https://api.dictionaryapi.dev/api/v2/entries/en/<word>
 	try:
-		x = request.urlopen('https://api.dictionaryapi.dev/api/v2/entries/en_US/'+word)
-		source = x.read()
-		data = json.loads(source)
-		return data
-		#show(data)   
-	except HTTPError as e:
-		return None
-	except URLError as e:
+		x = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en/'+'brothe')
+		data = x.json()
+		return data 
+	except:
 		return None
